0719ae17880f358e7bc395ed15aa3c7cd2fc2608
illTushar <gtushar7668@gmail.com>
2024-06-25 12:30:59 +0530

    25.6.24x2

---
+++ Object_of_effect/object_of_effect.py
@@ -0,0 +1,25 @@
+import pandas as pd
+import re
+
+
+def process_object_of_effect(sentence):
+    match = re.search(r'of (.*?) can be', sentence, re.IGNORECASE)
+    if match:
+        return match.group(1)
+    return None
+
+
+if __name__ == '__main__':
+    for i in range(1, 9):
+        file_path = fr'C:\Users\gtush\Desktop\FinalCsv\complete_file_splits\complete_extract_data_file2_part{i}.csv'
+        # Read the CSV file
+        read_csv_file = pd.read_csv(file_path)
+
+        # Apply the function to the dataframe
+        read_csv_file['object_of_effect'] = read_csv_file['remaining_sentence'].apply(process_object_of_effect)
+
+        # Display the dataframe
+        print(read_csv_file)
+
+        # # Optionally, save the modified dataframe to a new CSV file
+        read_csv_file.to_csv(file_path, index=False)
---
+++ Object_of_effect/replace_effect_with_object_of_effect.py
@@ -0,0 +1,21 @@
+import pandas as pd
+
+
+def process_object_of_effect(read_file_csv):
+    # Update 'Effect' with 'object_of_effect' values where they are not NaN
+    read_file_csv['Effect'] = read_file_csv['object_of_effect'].combine_first(read_file_csv['Effect'])
+    return read_file_csv
+
+
+if __name__ == '__main__':
+    for i in range(1, 9):
+        try:
+            file_path = fr'C:\Users\gtush\Desktop\FinalCsv\complete_file_splits\complete_extract_data_file2_part{i}.csv'
+            read_file_csv = pd.read_csv(file_path)
+
+            read_file_csv = process_object_of_effect(read_file_csv)
+
+            read_file_csv.to_csv(file_path, index=False)
+            print(f'Processed: {file_path}')
+        except Exception as e:
+            print(f'Error processing {file_path}: {e}')
